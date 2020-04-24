0dbc48ca574d6be2ba6d430bdb6003610830d55d
Mashud Ahmed <mashudahmed1980@gmail.com>
2020-04-24 18:38:12 -0400

    Added Field, Constructor at emptyController renamed StudentCotroller

--- ASP_Core_EF/Controllers/StudentController.cs
+++ ASP_Core_EF/Controllers/StudentController.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASP_Core_EF.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_Core_EF.Controllers
 {
     public class StudentController : Controller
     {
+        //Fields
+        private readonly IStudent _Student;
+
+        //Constructor
+        public StudentController(IStudent _IStudent)
+        {
+            _Student = _IStudent;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_Student.GetStudents);
         }
     }
 }
