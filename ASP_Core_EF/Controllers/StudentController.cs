e8f11e563b32f0355b6254312e1c7d854334d583
Mashud Ahmed <mashudahmed1980@gmail.com>
2020-04-24 19:27:19 -0400

    Added Index.cshtml and Create.cshtml from StudentController

--- ASP_Core_EF/Controllers/StudentController.cs
+++ ASP_Core_EF/Controllers/StudentController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASP_Core_EF.Models;
 using ASP_Core_EF.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +10,8 @@ namespace ASP_Core_EF.Controllers
 {
     public class StudentController : Controller
     {
-        //Fields
         private readonly IStudent _Student;
 
-        //Constructor
         public StudentController(IStudent _IStudent)
         {
             _Student = _IStudent;
@@ -20,6 +19,16 @@ namespace ASP_Core_EF.Controllers
         public IActionResult Index()
         {
             return View(_Student.GetStudents);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Student model)
+        {
+            return View(model);
         }
     }
 }
