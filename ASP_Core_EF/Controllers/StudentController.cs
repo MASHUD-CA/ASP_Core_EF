910eee2948caa53a902981d943a09975c4f02a8c
Mashud Ahmed <mashudahmed1980@gmail.com>
2020-04-25 18:48:10 -0400

    Added Delete (with Confirmation) at StudentController & Created Delete.cshtml at Views-Student

--- ASP_Core_EF/Controllers/StudentController.cs
+++ ASP_Core_EF/Controllers/StudentController.cs
@@ -41,5 +41,16 @@ namespace ASP_Core_EF.Controllers
             }
             return View(model);
         }
+        [HttpGet]
+        public IActionResult Delete(int? Id)
+        {
+            Student model = _Student.GetStudent(Id);
+            return View(model);
+        }
+        public IActionResult DeleteConfirm(int? Id)
+        {
+            _Student.Remove(Id);
+            return RedirectToAction("Index");
+        }
     }
 }
