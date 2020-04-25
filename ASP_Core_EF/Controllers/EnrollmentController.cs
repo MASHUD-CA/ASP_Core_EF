548aef7252d7a69e8481da105e2177eb654082c8
Mashud Ahmed <mashudahmed1980@gmail.com>
2020-04-25 19:32:25 -0400

    Created Field, Constructor, Created method at EnrollmentController.cs

--- ASP_Core_EF/Controllers/EnrollmentController.cs
+++ ASP_Core_EF/Controllers/EnrollmentController.cs
@@ -2,14 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASP_Core_EF.Models;
+using ASP_Core_EF.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_Core_EF.Controllers
 {
     public class EnrollmentController : Controller
     {
+        //field
+        private readonly IEnrollment _Enrollment;
+
+        //Constructor
+        public EnrollmentController(IEnrollment _IEnrollment)
+        {
+            _Enrollment = _IEnrollment;
+        }
+
         public IActionResult Index()
         {
+            return View(_Enrollment.GetEnrollments);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Enrollment model)
+        {
+            if (ModelState.IsValid)
+            {
+                _Enrollment.Add(model);
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
