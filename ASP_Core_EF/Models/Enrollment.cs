a8be747c768c2985c1aaa9f0762cbe5844c39dfe
Mashud Ahmed <mashudahmed1980@gmail.com>
2020-04-25 23:35:51 -0400

    Added Error Messages and Validation for Enrollment Class.

--- ASP_Core_EF/Models/Enrollment.cs
+++ ASP_Core_EF/Models/Enrollment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -19,11 +20,25 @@ namespace ASP_Core_EF.Models
     {
         [Key]
         public int EnrollmentId { get; set; }
-        public int StudentId { get; set; }
-        public int CoutseId { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndTime { get; set; }
 
+        //Adding Message & validation
+        [Required(ErrorMessage ="Student is Required")]
+        [DisplayName("Student Name")]
+        public int StudentId { get; set; }
+
+        [Required(ErrorMessage = "Course is Required")]
+        [DisplayName("Course Name")]
+        public int CourseId { get; set; }
+
+        [Required(ErrorMessage = "Start Date is Required")]
+        [DisplayName("Start Date")]
+        public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = "End Date is Required")]
+        [DisplayName("End Date")]
+        public DateTime EndDate { get; set; }
+
+        [DisplayFormat(NullDisplayText ="No Grade")]
         // Grade = an enumerated type (? in front of Grade = can be null )
         public Grade? Grade { get; set; }
     }
